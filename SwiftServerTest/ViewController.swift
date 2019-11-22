b2897d67da29e9236bfdd4cb1628fff1ec22a01b
tyler <kimtaesoo188@gmail.com>
2019-11-23 06:17:02 +0900

    Initial Commit

---
+++ SwiftServerTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftServerTest
+//
+//  Created by tyler on 2019/11/23.
+//  Copyright © 2019 tyler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
